7637e88e4d5b30a7112a53bc9cdd601cb1270ae6
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-06-29 20:44:12 +0200

    début page profil

--- src/components/views/Profile.jsx
+++ src/components/views/Profile.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+// import React, { useState } from 'react';
+
 import MainLayout from '../layouts/MainLayout';
 import './Profile.css';
 
 function Profile() {
+  // const [selected, setSelected] = useState(false);
   return (
     <div>
       <MainLayout>
         <h1>Profile</h1>
-        <h2>Mes préférences</h2>
-        <h3>Mes centres d&rsquo;intérêts</h3>
+        <h2>My preferences</h2>
+        <h3>My interests</h3>
         <div className="container">
           <div className="interet" />
           <div className="interet" />
           <div className="interet" />
           <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
         </div>
-        <h3>Mon rôle:</h3>
-        <p>Suis-je un mentor ? </p>
+        <h3>My role:</h3>
+        <form>
+          <p>
+            {' '}
+            Am I a mentor ? <br />
+            <label htmlFor="yes">
+              <input type="radio" value="yes" name="bool" /> Yes
+            </label>
+            <br />
+            <input type="radio" value="no" name="bool" /> No
+            <br />
+          </p>
+        </form>
       </MainLayout>
     </div>
   );

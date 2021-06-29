014768efdbdf82b16a416a5e5fc09a47482ce1f1
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-06-29 10:52:04 +0200

    début page profile

--- src/components/views/Profile.jsx
+++ src/components/views/Profile.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
+import './Profile.css';
 
 function Profile() {
   return (
     <div>
       <MainLayout>
         <h1>Profile</h1>
+        <h2>Mes préférences</h2>
+        <h3>Mes centres d&rsquo;intérêts</h3>
+        <div className="container">
+          <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
+          <div className="interet" />
+        </div>
+        <h3>Mon rôle:</h3>
+        <p>Suis-je un mentor ? </p>
       </MainLayout>
     </div>
   );

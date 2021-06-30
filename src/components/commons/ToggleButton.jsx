edc9ba4245929c6c078044c77349879fb91a0e43
clepi <clepirault@gmail.com>
2021-06-30 09:41:34 +0200

    local storage

--- src/components/commons/ToggleButton.jsx
+++ src/components/commons/ToggleButton.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ToggleButton.css';
 
 function ToggleButton(props) {
   const { title } = props;
+  const [isMentor, setIsMentor] = useState(false);
+  const handleClick = () => {
+    setIsMentor(!isMentor);
+  };
+  localStorage.setItem('is mentor', isMentor);
   return (
     <div>
       <span className="toggle">{title}</span>
       <label htmlFor="toggle" className="switch">
-        <input id="toggle" name="toggle" type="checkbox" />
+        <input
+          id="toggle"
+          name="toggle"
+          type="checkbox"
+          onClick={handleClick}
+        />
         <span className="slider" />
       </label>
     </div>
--- src/components/views/Profile.jsx
+++ src/components/views/Profile.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 
 function Profile() {
+  const isMentor = localStorage.getItem('is mentor');
   return (
     <div>
       <MainLayout>
         <h1>Profile</h1>
+        <p>{isMentor ? 'Vous etes mentor' : 'vous etes pas mentor'}</p>
         <Link to="/">Settings</Link>
       </MainLayout>
     </div>

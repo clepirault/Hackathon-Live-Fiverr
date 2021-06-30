c78c312a0dfcd29dcab2023e98151402860adade
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-06-30 11:15:30 +0200

    selection au clic

--- src/components/commons/DetailTopic.jsx
+++ src/components/commons/DetailTopic.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { useState } from 'react';
 import '../views/Settings.css';
 
 function DetailTopic(props) {
   const { topic, image, value } = props;
+  const [selected, setSelected] = useState(false);
 
   const handleClick = () => {
     let topics = localStorage.getItem('topics');
@@ -18,10 +20,11 @@ function DetailTopic(props) {
       topics = topics.filter((element) => element !== value);
     }
     localStorage.setItem('topics', JSON.stringify(topics));
+    setSelected(!selected);
   };
 
   return (
-    <div className="detailTopic">
+    <div className={selected ? 'detailTopicSelected' : 'detailTopic'}>
       <img
         src={image}
         alt={topic}
